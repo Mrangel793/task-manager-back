f4587e34af30da1501c0498df045a21a2d581870
Marly Rangel <129441027+Mrangel793@users.noreply.github.com>
2026-01-27 16:19:58 -0500

    Refactor UserSeeder to create Supervisor and Operador users with dynamic data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -27,7 +27,50 @@ class UserSeeder extends Seeder
 
         $this->command->info('Admin user created: ' . $admin->phone);
 
+        // Create Supervisor users
+        $supervisors = [
+            [
+                'phone' => '+573002222222',
+                'email' => 'supervisor1@taskmanager.local',
+                'name' => 'Supervisor 1',
+                'password' => 'Super123!',
+            ],
+            [
+                'phone' => '+573003333333',
+                'email' => 'supervisor2@taskmanager.local',
+                'name' => 'Supervisor 2',
+                'password' => 'Super123!',
+            ],
+        ];
 
+        foreach ($supervisors as $supervisorData) {
+            $supervisor = User::create([
+                'phone' => $supervisorData['phone'],
+                'email' => $supervisorData['email'],
+                'name' => $supervisorData['name'],
+                'password' => $supervisorData['password'],
+                'role' => 'Supervisor',
+                'is_active' => true,
+            ]);
+            $supervisor->assignRole('Supervisor');
+
+            $this->command->info('Supervisor user created: ' . $supervisor->phone);
+        }
+
+        // Create Operador users
+        for ($i = 4; $i <= 8; $i++) {
+            $operador = User::create([
+                'phone' => '+57300444444' . $i,
+                'email' => 'operador' . ($i - 3) . '@taskmanager.local',
+                'name' => 'Operador ' . ($i - 3),
+                'password' => 'Opera123!',
+                'role' => 'Operador',
+                'is_active' => true,
+            ]);
+            $operador->assignRole('Operador');
+
+            $this->command->info('Operador user created: ' . $operador->phone);
+        }
 
         $this->command->info('All users created successfully!');
     }
